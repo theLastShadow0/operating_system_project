78d2eb90d2d10d78151752163c562f39372bedf9
JairAcosta83 <jairacosta2003@gmail.com>
2024-09-25 09:30:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main() {
 
-  cout << "Hello World!" << endl;
+  //BOOTING UP THE SYSTEM
+  // 
+  // When power initialized on system, execution starts at a fixed memory location
+  // 
+  //		Firmware ROM used to hold initial boot code (bootstrap loader)
+  // 
+  //Small piece of code - bootstrap loader, stored in ROM or EEPROM located the kernel, loads it into memory, and starts it
+  //
+  //kernel loads and system is then running
 
+  return 0;
 }
