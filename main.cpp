b02b795fd239568dc472869d7baabce3760a451a
Andres <andreslopezdelagarza@gmail.com>
2024-09-29 15:19:45 -0500

    updated main

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-  //BOOTING UP THE SYSTEM
-  // 
-  // When power initialized on system, execution starts at a fixed memory location
-  // 
-  //		Firmware ROM used to hold initial boot code (bootstrap loader)
-  // 
-  //Small piece of code - bootstrap loader, stored in ROM or EEPROM located the kernel, loads it into memory, and starts it
-  //
-  //kernel loads and system is then running
-
-  /*
-
-  IMPLEMENTATION:
-    Make a class named ROM 
-    Get "bootstrap loader" from class
-    Bootstrap loader will contain the address of the "kernel"
-    Kernel "loads" (simulate that it loaded) and system is "running"
-  
-  */
-cout << "Welcome to TAMIU OS" << endl; 
-
-  
-  //LOGIN AUTHENTICATION
+ 
+//LOGIN AUTHENTICATION
   //
   //Like any other computer, it would require a username and password
   //
@@ -48,45 +25,53 @@ cout << "Welcome to TAMIU OS" << endl;
   
   */
 
-  //For the system login is it simply asking for a username and password from the user.
-  //It does not check if there is an account in the system/ Doesnt save accounts.
-    int loginChoice;
-    string userName;
-    string passWord;
-    
-    cout << "Press '1' to login to an existing account" << endl;
-    cout << "Press '2' to create a new account" << endl;
-    cout << "Press '0' to turn shut down" << endl;
-    cin >> loginChoice;
 
 
-    switch(loginChoice){
-    case 0:
-        cout << "Shutting down..." << endl;
-        return 0;
-    case 1:
-        cout << "Username: " << endl;
-        cin >> userName;
-        cout << "Password: " << endl;
-        cin >> passWord;
-        cout << "Welcome back " << userName << endl;
-        break;
-    case 2:
-        cout << "New Username: " << endl;
-        cin >> userName;
-        cout << "New Password: " << endl;
-        cin >> passWord;
-        cout << "Welcome " << userName << endl;
-        break;
-    }
-    
   //PROCESS CREATION AND TERMINATION
+  //
+  //Create a class for the Operating System which will have the login prompt and processes. 
+  //
+  //For the system login is it simply asking for a username and password from the user.
+  //It does not check if there is an account in the system/doesn't save accounts.
   //
   //For the process creation I made a menu where the user can choose what program they want to execute.
   //The menu will keep on looping until the user decides to end the program by signing out.
-  //Work is needed so the user can run a program and decide to end that program and go back to the menu.
-  //
+  //Work is needed so the user can terminate the program and choose to execute another one.
+  
+class OperatingSystem{
+  public:
+    string userName;
+    string passWord;
 
+    void login(){
+      int loginChoice;
+      cout << "Press '1' to login to an existing account" << endl;
+      cout << "Press '2' to create a new account" << endl;
+      cout << "Press '0' to turn shut down" << endl;
+      cin >> loginChoice;
+
+      switch(loginChoice){
+      case 0:
+          cout << "Shutting down..." << endl;
+          exit(0);
+      case 1:
+          cout << "Username: " << endl;
+          cin >> userName;
+          cout << "Password: " << endl;
+          cin >> passWord;
+          cout << "Welcome back " << userName << endl;
+          return menu();
+      case 2:
+          cout << "New Username: " << endl;
+          cin >> userName;
+          cout << "New Password: " << endl;
+          cin >> passWord;
+          cout << "Welcome " << userName << endl;
+          return menu();
+      }
+  }
+
+  void menu(){
     int menuChoice;
     do{
         cout << "Please choose a program to execute on your OS: " << endl;
@@ -100,7 +85,7 @@ cout << "Welcome to TAMIU OS" << endl;
         switch(menuChoice){
             case 0:
                 cout << "Signing out...\n" << endl;
-                return main();
+                return login();
             case 1:
                 cout << "Running Google Chrome..." << endl;
                 break;
@@ -114,6 +99,41 @@ cout << "Welcome to TAMIU OS" << endl;
                 cout << "Running Microsoft PowerPoint" << endl;
                 break;
         }
-    }while (menuChoice != 0);
-    return 0;
+    }
+    while (menuChoice != 0);
+  }
+};
+
+int main() {
+
+  //BOOTING UP THE SYSTEM
+  // 
+  // When power initialized on system, execution starts at a fixed memory location
+  // 
+  //		Firmware ROM used to hold initial boot code (bootstrap loader)
+  // 
+  //Small piece of code - bootstrap loader, stored in ROM or EEPROM located the kernel, loads it into memory, and starts it
+  //
+  //kernel loads and system is then running
+
+  /*
+
+  IMPLEMENTATION:
+    Make a class named ROM 
+    Get "bootstrap loader" from class
+    Bootstrap loader will contain the address of the "kernel"
+    Kernel "loads" (simulate that it loaded) and system is "running"
+  
+  */
+ 
+//Create object of OperatingSystem Class 
+OperatingSystem tamiuOS;
+
+cout << "Welcome to TAMIU OS" << endl;
+//Call of functions
+tamiuOS.login();
+
+tamiuOS.menu();
+
+return 0;
 }
