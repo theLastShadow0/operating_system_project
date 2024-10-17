5ecbf6bf36a230e8d9330dbaa5fab371a4d09e75
Andres Lopez <163787540+theLastShadow0@users.noreply.github.com>
2024-10-16 21:48:23 -0500

    Update Pthread.cpp

--- Pthread.cpp
+++ Pthread.cpp
@@ -17,12 +17,15 @@ struct Student {
 double percentage(int size, Student array[]) {
 
 	double passed = 0;
+	//keeps track on how many students passed.
 
 	for (int i = 0; i < size; i++) { 
 		if (array[i].result == 1) {
 			passed++;
 		}
 	}
+	//In this for loop it checks the result array
+	//If its equal to one the passed variable is increased
 
 	return ((passed / size) * 100);
 
@@ -30,7 +33,8 @@ double percentage(int size, Student array[]) {
 
 int main() {
 
-	int const size = 6;
+	int const size = 6; 
+	//number of students
 
 	Student arr[size];
 
@@ -49,14 +53,18 @@ int main() {
 
 		while (!MyReadFile.eof()) {
 			getline(MyReadFile, myText, ' ');
-			//cout << myText << endl; 
-
+			//cout << myText << endl;
+			//This will read the file until it detects a space.
+			//In this case it is getting the student ID.
 			stringstream(myText) >> arr[i].id; 
+			//stores the text into the array.
 
 			getline(MyReadFile, myText, '\n');
 			//cout << myText << endl;
-
+			//Continues to read the file after the space
+			//which will get the result of pass or fail.
 			stringstream(myText) >> arr[i].result; 
+			//stores the result into the array
 
 			i++;
 		}
