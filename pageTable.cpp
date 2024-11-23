7bc8827c6b9f5e64c8cb1f56c9871aa1a167c956
Andres <andreslopezdelagarza@gmail.com>
2024-11-23 16:17:52 -0600

    completed page table

--- pageTable.cpp
+++ pageTable.cpp
@@ -2,57 +2,69 @@
 #include <unordered_map>
 #include <string>
 #include <iomanip>
-#include <sstream>
+
+using namespace std;
 
 class logicalMemory {
 public:
-    std::string name; // Program name
-    double virtualAddress; // Virtual address in decimal
+    string name; // Program name
+    int virtualAddress; // Virtual address in decimal
     
-    //initializes name as empty string, and virtualAddress as 0
-    logicalMemory(): name(""), virtualAddress(0) {}
+    logicalMemory(): name(""), virtualAddress(0) {} //initializes name as empty string, and virtualAddress as 0
 
-    logicalMemory(const std::string& name, double virtualAddress)
+    logicalMemory(const string& name, int virtualAddress)
         : name(name), virtualAddress(virtualAddress) {}
-
-    //virtual address in hexadecimal
-    std::string getHexAddress() const {
-        std::stringstream ss;
-        ss << std::hex << std::uppercase << static_cast<int>(virtualAddress);
-        return ss.str();
-    }
 };
 
-
-class PageTable{
+class LogicalTable{
 private:
-    std::unordered_map<std::string, logicalMemory> pageTable;
+    unordered_map<string, logicalMemory> logicalTable; //program name as the key, and virtual address as value
+    unordered_map<int,int>pageTable; //page number is set as the key, and frame number as the value
 
 public: //constructor
-    PageTable(){}
-    
-    void addProgram(const std:: string& name, double virtualAddress){
-        pageTable[name] = logicalMemory(name, virtualAddress);
+
+    void addProgram(const string& name, int virtualAddress, int frameNumber){
+        int pageNumber = virtualAddress / 4096; 
+        
+        pageTable[pageNumber] = frameNumber;
+        logicalTable[name] = logicalMemory(name, virtualAddress);
     }
-    
-    void displayPageTable() const {
-        for (const auto& entry : pageTable){
-            std:: cout << "Program: " << entry.second.name << ", Virtual Address: 0x" << entry.second.getHexAddress()<< std::endl;
+
+    int translate(int virtualAddress, int pageSize)const{
+        int pageNumber = virtualAddress / pageSize;
+        int offset = virtualAddress % pageSize;
+
+        if(pageTable.find(pageNumber)== pageTable.end()){
+            cout << "Error: Page number not found in page table." << endl;
+            return -1;
+        }
+
+        int frameNumber = pageTable.at(pageNumber);
+        int physicalAddress = frameNumber * pageSize + offset;
+        return physicalAddress;
+    }
+
+    void displayLogicalTable() const {
+        for (const auto& entry: logicalTable){
+
+            int physicalAddress = translate(entry.second.virtualAddress, 4096);
+            cout<< entry.second.name << " has a virtual address of 0x" << hex << entry.second.virtualAddress<< " and got translated to physical address " << "0x" << hex << physicalAddress << endl;
         }
     }
 };
 
 
 int main(){
+
+    LogicalTable pt;
     
-    PageTable pt;
-    
-    pt.addProgram("Google Chrome", 1193200); //virtual address will be translated to hex at the end
-    pt.addProgram("Microsoft Word", 1193201);
-    pt.addProgram("File Explorer", 1193202);
-    pt.addProgram("Microsoft PowerPoint", 1193203);
-    
-    pt.displayPageTable();
+    //unordered map has the name of the program, virtual address in decimal, and the frame number
+    pt.addProgram("Google Chrome", 1193200,5); 
+    pt.addProgram("Microsoft Word", 1193201,6);
+    pt.addProgram("File Explorer", 1193202,7);
+    pt.addProgram("Microsoft PowerPoint", 1193203,8);
+
+    pt.displayLogicalTable();
     
     return 0;
 }
